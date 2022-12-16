80bcdf5859f80124ba509e1b2de21d0cb0b2e108
Martin Stancsics <martin.stancsics@gmail.com>
2022-12-16 15:25:27 +0100

    Continue the bargaining case

--- src/sympy/bargaining_case.py
+++ src/sympy/bargaining_case.py
@@ -1,4 +1,4 @@
-from sympy import Symbol, integrate, init_printing, simplify, factor, solve
+from sympy import Symbol, integrate, init_printing, simplify, factor, solve, log
 
 init_printing()
 
@@ -23,17 +23,11 @@ pi_F
 pi_F_t = pi_F - I_F * N_F
 solve(pi_F_t, N_F)  # :(
 
-d_N_F = simplify(pi_F_t.diff(N_F))
-d_N_P = simplify(pi_F_t.diff(N_P))
-
-d_N_F_N_P = factor(simplify(d_N_F / d_N_P))
-d_N_F_N_P
-
-pi_F_simplified = mu * (
-    (N_P * V_P + N_F * V_F) / (N_P * V_P + N_F * V_F + 1)
-    - 1
-    - 1 / (N_F * V_F)
+pi_F_t_alt = mu * (
+    log(1 + (N_F * V_F) / (N_P * V_P + 1)) / (N_F * V_F)
+    - 1 / (N_P * V_P + N_F * V_F + 1)
 ) - I_F * N_F
-d_N_F_N_P_simplified = simplify(
-    pi_F_simplified.diff(N_F) / pi_F_simplified.diff(N_P)
+d_N_F_N_P = simplify(
+    pi_F_t_alt.diff(N_F) / pi_F_t_alt.diff(N_P)
 )
+
