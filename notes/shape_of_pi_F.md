37b018358e724676abf0fc4f9ead110bbba75132
Martin Stancsics <martin.stancsics@gmail.com>
2022-12-16 18:17:05 +0100

    Add some notes about proving decreasing welfare

---
+++ notes/shape_of_pi_F.md
@@ -0,0 +1,8 @@
+# The platform's product pricing decision
+
+Main issue: $pi_F$ must have this specific nice hump shape in order to prove uniqueness of the equilibrium and that an increase in $N_P$ is welfare-decreasing.
+
+## Ideas I can use
+ - $V_P$ and $\mu$ are just scaling factors and $N_P$ and $V_P$ always appear together, therefore the function has essentially one parameter $\implies$ WLOG $\mu = V_F = 1$ and $\tilde V_P = N_P V_P$.
+ - In terms of welfare, $N_F$ is not important, only $N_F V_F$ is relevant.
+ - Is showing the necessary property for $$ \frac{\log(N_F + 1)}{\N_F} - \frac{1}{N_F + 1} $$ sufficient?
--- src/sympy/bargaining_case.py
+++ src/sympy/bargaining_case.py
@@ -8,7 +8,7 @@ V_P = Symbol('V_P')
 
 I_F = Symbol('I_F')
 
-N_F = Symbol('N_f')
+N_F = Symbol('N_F')
 N_P = Symbol('N_P')
 n = Symbol('n')
 
@@ -21,13 +21,19 @@ pi_P
 pi_F
 
 pi_F_t = pi_F - I_F * N_F
-solve(pi_F_t, N_F)  # :(
+# solve(pi_F_t, N_F)  # :(
 
-pi_F_t_alt = mu * (
+pi_F_alt = mu * (
     log(1 + (N_F * V_F) / (N_P * V_P + 1)) / (N_F * V_F)
     - 1 / (N_P * V_P + N_F * V_F + 1)
-) - I_F * N_F
+)
+pi_F_t_alt = pi_F_alt + I_F * N_F
 d_N_F_N_P = simplify(
     pi_F_t_alt.diff(N_F) / pi_F_t_alt.diff(N_P)
 )
 
+simplify((pi_F_alt).diff(N_F))
+simplify((pi_F_alt).diff(N_F).diff(N_F))
+simplify((pi_F_alt).diff(N_F).diff(N_F).diff(N_F))
+
+(2 * log((N_F * V_F) / (N_P * V_P + 1) + 1)).diff(N_F)  # type: ignore
