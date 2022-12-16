cce906f2f5ad0c7889500eef5fadf5556fc8f37c
Martin Stancsics <martin.stancsics@gmail.com>
2022-12-16 14:53:52 +0100

    Try IFT

--- src/sympy/bargaining_case.py
+++ src/sympy/bargaining_case.py
@@ -1,5 +1,4 @@
-import sympy
-from sympy import Symbol, integrate, init_printing, simplify
+from sympy import Symbol, integrate, init_printing, simplify, factor, solve
 
 init_printing()
 
@@ -21,4 +20,20 @@ pi_F = simplify(v.subs({n: N_F}) - pi_P)
 pi_P
 pi_F
 
-simplify(pi_F.diff(N_P))
+pi_F_t = pi_F - I_F * N_F
+solve(pi_F_t, N_F)  # :(
+
+d_N_F = simplify(pi_F_t.diff(N_F))
+d_N_P = simplify(pi_F_t.diff(N_P))
+
+d_N_F_N_P = factor(simplify(d_N_F / d_N_P))
+d_N_F_N_P
+
+pi_F_simplified = mu * (
+    (N_P * V_P + N_F * V_F) / (N_P * V_P + N_F * V_F + 1)
+    - 1
+    - 1 / (N_F * V_F)
+) - I_F * N_F
+d_N_F_N_P_simplified = simplify(
+    pi_F_simplified.diff(N_F) / pi_F_simplified.diff(N_P)
+)
